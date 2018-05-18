72d85d65853f6bd5dbd029af285c6bf07fa243bb
Zangles <azuresky07@gmail.com>
2018-05-18 00:52:09 -0300

    login and template

---
+++ resources/views/layouts/login.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('/font-awesome/css/font-awesome.css') }}" rel="stylesheet">
+
+    <link href="{{ asset('/css/animate.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/style.css') }}" rel="stylesheet">
+
+</head>
+
+<body class="gray-bg">
+@yield('content')
+
+
+<!-- Mainly scripts -->
+<script src="{{ asset('/js/jquery-2.1.1.js') }}"></script>
+<script src="{{ asset('/js/bootstrap.min.js') }}"></script>
+
+</body>
+
+</html>
+
